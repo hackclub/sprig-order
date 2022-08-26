31342d708a9c81fa7c2ba2a671355b9bbaf43fcb
Max Wofford <max@maxwofford.com>
2022-08-25 23:38:46 -0400

    Fix typo in callback URL

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -4,7 +4,7 @@ export default async (req, res) => {
   // redirect the user to the github oauth page
   const url = new URL('https://github.com/login/oauth/authorize')
   url.searchParams.set('client_id', process.env.GITHUB_CLIENT_ID)
-  url.searchParams.set('redirect_uri', 'https://sprig-order.hackclub.com/callback')
+  url.searchParams.set('redirect_uri', 'https://sprig-order.hackclub.com/api/callback')
 
   res.redirect(302, url.toString())
 }
