bde02c80cc62d69dea7a74d706e7ff9873d7e36a
Max Wofford <max@maxwofford.com>
2022-08-26 00:21:28 -0400

    Add optional PR param

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,10 +1,12 @@
 // start oauth flow with github
 
 export default async (req, res) => {
+  const { pr } = req.query
   // redirect the user to the github oauth page
   const url = new URL('https://github.com/login/oauth/authorize')
   url.searchParams.set('client_id', process.env.GITHUB_CLIENT_ID)
   url.searchParams.set('redirect_uri', 'https://sprig-order.hackclub.com/api/callback')
+  url.searchParams.set('state', pr)
 
   res.redirect(302, url.toString())
 }
