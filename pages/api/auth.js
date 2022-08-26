8cd9c9f22c179424aec0096e4763706b221cb24f
Max Wofford <max@maxwofford.com>
2022-08-25 23:20:47 -0400

    Initial commit

---
+++ pages/api/auth.js
@@ -0,0 +1,10 @@
+// start oauth flow with github
+
+export default async (req, res) => {
+  // redirect the user to the github oauth page
+  const url = new URL('https://github.com/login/oauth/authorize')
+  url.searchParams.set('client_id', process.env.GITHUB_CLIENT_ID)
+  url.searchParams.set('redirect_uri', 'https://sprig-order.hackclub.com/callback')
+
+  res.redirect(302, url.toString())
+}
---
+++ pages/api/callback.js
@@ -0,0 +1,40 @@
+export default async (req, res) => {
+  // get the code from the query string
+  const { code } = req.query
+
+  // exchange the code for an access token
+  const url = new URL('https://github.com/login/oauth/access_token')
+  url.searchParams.set('client_id', process.env.GITHUB_CLIENT_ID)
+  url.searchParams.set('client_secret', process.env.GITHUB_CLIENT_SECRET)
+  url.searchParams.set('code', code)
+  const { accessToken } = await fetch(url, {
+    method: 'POST',
+  })
+
+  // use access token to get username from github
+  const { login } = await fetch('https://api.github.com/user', {
+    headers: {
+      Authorization: `token ${accessToken}`,
+    }
+  })
+
+  // record the access token in the database
+  const airtableID = await fetch('https://api2.hackclub.com/v0.2/Sprig Waitlist/Authentication', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.AIRBRIDGE_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      'GitHub Token': accessToken,
+      'GitHub Username': login
+    })
+  }).then(r => r.json()).then(r => r.id)
+
+  // redirect the user to the submission form
+  const formUrl = new URL('https://airtable.com/shrePmF41sbxEXqkZ')
+  formUrl.searchParams.set('prefill_GitHub Username', login)
+  formUrl.searchParams.set('prefill_Authentication ID', airtableID)
+
+  res.redirect(302, formUrl.toString())
+}
